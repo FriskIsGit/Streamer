b973bdf61c11bbf655bcfee005ea8bd7cfa9e39b
FriskIsGit <friskisgit@users.noreply.github.com>
2022-09-12 22:36:52 +0200

    renaming incident

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -2,7 +2,7 @@ package app;
 
 public class Main{
     public static void main(String[] args){
-        Streamer app = new Streamer();
+        InitialFrame app = new InitialFrame();
         app.setup();
     }
 }
